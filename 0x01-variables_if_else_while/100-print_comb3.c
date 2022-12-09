5f3a1a0c53837ebabf6d3b7f5659e7bb4c5ef3ac
amrsayedcode <amracademycode@gmail.com>
2022-12-09 07:17:44 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,6 @@ int main(void)
 {
 	int ones = '0';
 	int tens = '0';
-	
 	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
 	{
 		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
@@ -27,7 +26,6 @@ int main(void)
 			}
 		}
 	}
-	
 	putchar('\n');
 	return (0);
 }
